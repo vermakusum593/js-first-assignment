32dd3b9d7c08eb23da72f7586570850f22666f32
vermakusum593 <vermakusum593@gmail.com>
2024-10-15 17:45:33 +0200

    added main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+//string
+console.log("Hello, World!");
+
+//variables
+let firstName = "Kusum";
+let lastName = "verma";
+let age = "27";
+let favouriteColor = "Blue";
+console.log(
+  `My name is ${firstName}, I'm ${age} years old and my favourite color is ${favouriteColor}`
+);
+console.log(`Type of firstName ${typeof firstName}`);
+
+//arithmetic operations
+let x = 22;
+let y = 11;
+console.log(`Sum: ${x + y}`);
+console.log(`Subtraction: ${x - y}`);
+console.log(`Product: ${x * y}`);
+console.log(`Division: ${x / y}`);
+
+
+
